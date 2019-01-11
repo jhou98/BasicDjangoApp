fb278866a5c043b107079ace8d281a8638a9c7e7
Jack Hou <jackhou1998@gmail.com>
2019-01-11 14:50:25 -0800

    Modified how data is stored

--- graphs/admin.py
+++ graphs/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from .models import EVData
 
 admin.site.register(EVData)
--- graphs/models.py
+++ graphs/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-"""
+class EVData(models.Model):
+    """
     Class that represents EV data and has the following: 
         1. Date - in Date format 
         2. Time - in Time format 
         3. kW - up to 999 with 3 decimal places
     See https://docs.djangoproject.com/en/2.1/ref/models/fields/#django.db.models.DateField for more details on the models
-"""
-class EVData(models.Model):
+    """
     data_date = models.DateField()
     data_time = models.TimeField() 
-    data_kW = models.DecimalField(max_digits = 6, decimal_places = 3)
+    data_kW = models.DecimalField(max_digits = 8, decimal_places = 3)
 
     """
         Returns a string of the following format
@@ -18,3 +18,8 @@ class EVData(models.Model):
     """
     def __str__(self):
         return "Date: " + self.data_date.strftime('%m/%d/%Y') + " Time: " + self.data_time.strftime('%H:%M:%S')  + " kW: " + str(self.data_kW)
+
+class EVData2(models.Model):
+    data_date = models.Date()
+    data_time = models.Time()
+    '00:00' = models.DecimalField(max_digits = 8, decimal_places = 3)
