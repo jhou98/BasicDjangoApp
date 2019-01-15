40f872b2d8048ff009753054040d1fa9893d79fd
Jack Hou <jackhou1998@gmail.com>
2019-01-15 11:11:13 -0800

    updated query.py

--- graphs/query.py
+++ graphs/query.py
@@ -18,10 +18,45 @@ __port = '3306'
 
 def createNewTable(pathtofile, tablename): 
     """
-    Uses Pandas to read a csv file to a dataframe object 
+    Uses Pandas to read a csv file to a dataframe object and stores it into our database
     :param str pathtofile: Path to file from our projects base directory
     :param str tablename: Name of the table we wish to input data into. \
-    IF tablename exists, it will be replaced
+    IF tablename exists, it will fail
+    """
+    df = pd.read_csv(__base+pathtofile)
+    
+    print("Finished reading csv")
+    print(df)
+    
+    engine = create_engine('mysql+mysqlconnector://'+ __user + ':' + __passw + '@' + __host + ':' + __port + '/' + __schema, echo=False)
+    print("Connected to mysql\n")
+
+    df.to_sql(con=engine, name=tablename, if_exists='fail')
+
+def updateTable(pathtofile, tablename):
+    """
+    Updates a table in our database by appending the new data into the table 
+    :param str pathtofile: Path to file from our projects base directory
+    :param str tablename: name of table to input data. \
+    IF tablename exists, we will append the data to the bottom \
+    OTHERWISE it will create a new table 
+    """
+    df = pd.read_csv(__base+pathtofile)
+    
+    print("Finished reading csv")
+    print(df)
+    
+    engine = create_engine('mysql+mysqlconnector://'+ __user + ':' + __passw + '@' + __host + ':' + __port + '/' + __schema, echo=False)
+    print("Connected to mysql\n")
+
+    df.to_sql(con=engine, name=tablename, if_exists='append')
+
+def replaceTable(pathtofile, tablename): 
+    """
+    Uses Pandas to read a csv file to a dataframe object and stores it into our database
+    :param str pathtofile: Path to file from our projects base directory
+    :param str tablename: Name of the table we wish to input data into. \
+    IF tablename exists, it will be replaced with the new data
     """
     df = pd.read_csv(__base+pathtofile)
     
