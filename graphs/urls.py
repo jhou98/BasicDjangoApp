24df68074a4d9b40f2ca3018016decba1f6f0f45
Jack Hou <jackhou1998@gmail.com>
2019-02-01 09:53:54 -0800

    Adding gauges for static data

--- graphs/urls.py
+++ graphs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path(r'max/<int:num_req>/',views.peakData, name='max'),
     path(r'date/<str:date_val>/',views.dateData, name='date'),
     path(r'api/data',views.ChartData.as_view(), name='chart' ),
+    path(r'api/data2',views.ErrData.as_view(), name='err'),
 ]
