1fb834f271fea9a41b0bf21b7bd843eae1ff9203
Jack Hou <jacklolz2012@hotmail.com>
2019-03-29 14:49:27 -0700

    realtime updates to all elements except sliding gauge

--- docs/graphs/dist/updates.js
+++ docs/graphs/dist/updates.js
@@ -14,25 +14,14 @@ function addDataChart(chart, curr_x, curr_y, pred_x, pred_y, pred_max, pred_min)
    
     //create the new arrays that will have null values so that total sizes are equal to the label size 
     var timestamp = curr_x.slice(0)
-    var evpwr = curr_y.slice(0)
+    var val = curr_y.slice(0)
     var predictedpwr = []
     var predictedmax = []
     var predictedmin = []
-    for (var x in curr_x) {
-        predictedpwr.push(null)
-        predictedmax.push(null)
-        predictedmin.push(null)
-    }
-    for (var x in pred_x) {
-        timestamp.push(pred_x[x])
-        evpwr.push(null)
-        predictedpwr.push(pred_y[x])
-        predictedmax.push(pred_max[x])
-        predictedmin.push(pred_min[x])
-    }
+    formatpredicted(curr_x, pred_x, pred_y, pred_max, pred_min, timestamp, val, predictedval, predictedmax, predictedmin)
 
-    chart.data.datasets[0].data = evpwr
-    chart.data.datasets[1].data = predictedpwr
+    chart.data.datasets[0].data = val
+    chart.data.datasets[1].data = predictedval
     chart.data.datasets[2].data = predictedmax
     chart.data.datasets[3].data = predictedmin
     chart.data.labels = timestamp
@@ -56,13 +45,7 @@ function updateChart(chart, timestamps, pwr, _url) {
             var predicted_val = []
             var predicted_max = []
             var predicted_min = []
-            var predicted_length = result.data.length - 1
-            for (var x in result.data) {
-                predicted_date.push((result.data[predicted_length - x][0]).slice(0, 16))
-                predicted_val.push(result.data[predicted_length - x][1])
-                predicted_max.push(result.data[predicted_length - x][2])
-                predicted_min.push(result.data[predicted_length - x][3])
-            }
+            parsepredicted(predicted_date, predicted_val, predicted_max, predicted_min, result.data)
             //Add most recent datapoint into our chart
             addDataChart(chart, timestamps, pwr, predicted_date, predicted_val, predicted_max, predicted_min)
         },
@@ -73,7 +56,7 @@ function updateChart(chart, timestamps, pwr, _url) {
 }
 
 /**
- * This function updates our Building Chart. 
+ * This function updates our Chart and gauges.  
  * 
  * @param {Chart} chart Chart object.  
  * @param {Gauge} daily_gauge Gauge referring to our daily gauge. 
@@ -83,10 +66,10 @@ function updateChart(chart, timestamps, pwr, _url) {
  * 
  * @returns {Array} returns an array of building power that can be used. 
  */
-function updateBuildingData(chart, daily_gauge, monthly_gauge, bd_url, pred_url) {
+function updateBuildingData(chart, daily_gauge, monthly_gauge, _url, pred_url) {
     var retval = []
     $.ajax({
-        url: bd_url,
+        url: _url,
         type: "GET",
         async: false,
         success: function (data) {
@@ -94,14 +77,7 @@ function updateBuildingData(chart, daily_gauge, monthly_gauge, bd_url, pred_url)
             var result = JSON.parse(data)
             var date = []
             var power = []
-            //length (as our objects are in Most recent->least recent)
-            var length = result.data.length - 1
-            for (var x in result.data) {
-                //slice the ISO-Format String 
-                //YYYY-mm-ddTHH:MM:SS.DDDZ -> YYYY-mm-ddTHH:MM:SS
-                date.push(result.data[length - x][0].slice(0, 16))
-                power.push(result.data[length - x][1])
-            }
+            parsedata(date, power, result.data)
             //Update our EV gauge and Charts
             daily_gauge.setValue(result.data[0][1])
             monthly_gauge.setValue(result.data[0][1])
@@ -113,6 +89,33 @@ function updateBuildingData(chart, daily_gauge, monthly_gauge, bd_url, pred_url)
         }
     });
     return retval
+}
+
+/**
+ * Function updates a Chart without gauges. 
+ * @param {Chart} chart Chart object. 
+ * @param {string} _url string for URL to obtain historical data in JSON format. 
+ * @param {string} pred_url string for URL to obtain predicted data in JSON format. 
+ */
+function updateData(chart, _url, pred_url) {
+    $.ajax({
+        url: _url,
+        type: "GET",
+        success: function (data) {
+            console.log("polling next vehicle point")
+            var result = JSON.parse(data)
+            var date = []
+            var val = []
+            parsedata(date, val, result.data)
+            //Update our EV gauge and Charts
+            daily_gauge.setValue(result.data[0][1])
+            monthly_gauge.setValue(result.data[0][1])
+            updateChart(chart, date, val, pred_url)
+        },
+        error: function (err_data) {
+            console.log("error", err_data)
+        }
+    });
 }
 
 /**
@@ -149,4 +152,78 @@ function updateBarData(chart, power){
     chart.data.datasets[0].data = power
     chart.update(); 
     console.log("updating bar data")
+}
+
+/**
+ * Function that helps us parse our predicted data 
+ * @note Assumes that the data array is a 2d array where the second dimension corresponds
+ * as follows: 
+ *  - [0]: date
+ *  - [1]: value
+ *  - [2]: max 
+ *  - [3]: min 
+ * 
+ * @param {Array} date array of dates 
+ * @param {Array} val array of values 
+ * @param {Array} max array of max error 
+ * @param {Array} min array of min error 
+ * @param {Array} data Data array to parse 
+ */
+function parsepredicted(date, val, max, min, data) {
+    var length = data.length - 1
+    for (var x in data) {
+        date.push((data[length - x][0]).slice(0, 16))
+        val.push(data[length - x][1])
+        max.push(data[length - x][2])
+        min.push(data[length - x][3])
+    }
+}
+
+/**
+ * Function that formats the predicted values with the current values 
+ * 
+ * @param {Array} curr_date array of current dates
+ * @param {Array} pred_date array of predicted dates
+ * @param {Array} pred_val array of predicted values 
+ * @param {Array} pred_max array of predicted max error
+ * @param {Array} pred_min array of predicted min error
+ * @param {Array} date array of dates for our graph
+ * @param {Array} val array of values for our graph
+ * @param {Array} pred array of predictions for our graph
+ * @param {Array} max array of max error for our graph
+ * @param {Array} min array of min error for our graph 
+ */
+function formatpredicted(curr_date, pred_date, pred_val, pred_max, pred_min, date, val, pred, max, min) {
+    for (var x in curr_date) {
+        pred.push(null)
+        max.push(null)
+        min.push(null)
+    }
+
+    for (var y in pred_date) {
+        date.push(pred_date[y])
+        val.push(null)
+        pred.push(pred_val[y])
+        max.push(pred_max[y])
+        min.push(pred_min[y])
+    }
+}
+
+/**
+ * Function that helps us parse our current data. 
+ * @note Assumes that the data array is a 2d array where the second dimension corresponds
+ * as follows: 
+ *  - [0]: date
+ *  - [1]: value
+ * 
+ * @param {Array} date Array of dates
+ * @param {Array} val Array of values 
+ * @param {Array} data Array of data to be parsed 
+ */
+function parsedata(date, val, data) {
+    var length = data.length - 1
+    for (var x in data) {
+        date.push((data[length - x][0]).slice(0, 16))
+        val.push(data[length - x][1])
+    }
 }
